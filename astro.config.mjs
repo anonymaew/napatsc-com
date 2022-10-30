36ac0e2ca75f8c7337d07b04afebec159ad0b15a
Napat Srichan <napatsrichan2001@gmail.com>
2022-10-29 22:40:11 -0700

    add default layout injection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+// injecting default layout
+const defaultLayoutPlugin = () => {
+  return (tree, file) => {
+    file.data.astro.frontmatter.layout = "/src/layouts/main.astro";
+  };
+};
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://napatsc.com",
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [defaultLayoutPlugin],
+  },
 });
